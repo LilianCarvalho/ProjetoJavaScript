8eaf053cfa283830cfafccbd27b2f0fe0c9b06ef
LilianCarvalho <liliancarol93@gmail.com>
2021-06-19 20:34:58 -0300

    Corrigindo o ex 3

--- Exercícios/ex-03/js.js
+++ Exercícios/ex-03/js.js
@@ -1,3 +1,4 @@
+/*
 var a = 
     [0,3,99,44, -1] 
 var b = 
@@ -7,14 +8,33 @@ var c =
 
 var listas = [a+b+c]
 
-listas.sort (ordenacao)
-function ordenacao (a,b){
+ listas.sort (ordenacao)
+    function ordenacao (a,b){
     return b-a
 }
+console.log(listas)
+*/
 
-for (var i = 0; i < listas.length; i++){
-    console.log(listas[i] )
+const juncao = (a, b, c) => {
+    const d = a.concat(b, c)
+    d.sort((b,a) => {
+        if(a > b) return 1;
+
+        if(a < b) return -1
+
+        return 0
+    })
+    return d
 }
+
+let a = [0, 3, 99, 44, -1] 
+let b = [4, 6, 9, 10, -349] 
+let c = [1, 1, 1, 99, 37] 
+
+console.log(juncao(a, b, c))
+
+//Melhorando o cod, e avançando nos estudos.
+
 
 
 
