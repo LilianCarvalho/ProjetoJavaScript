ae5d99e708ddfb8a6a3dde55d3936bd3edc05159
LilianCarvalho <liliancarol93@gmail.com>
2021-06-21 12:34:39 -0300

    Melhorando o ex4

--- Exercícios/ex-4/js.js
+++ Exercícios/ex-4/js.js
@@ -1,3 +1,4 @@
+/*
 soma=0
 for (i=0;i<100;i++)
 {
@@ -7,3 +8,14 @@ for (i=0;i<100;i++)
     document.write("\n i: " +i)
     }
 }
+*/
+
+var num = 0 
+while (num <=100) {
+    if (num %2 ==1){
+        document.write`${num} --`
+    }
+    ++num;
+}
+
+//Melhorando o código 
