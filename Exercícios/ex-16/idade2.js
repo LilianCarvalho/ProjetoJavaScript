a9dca1ee213e06b7fa20ff1880aba437e7b6ae19
Lilian Carvalho <lilian.carvalho@paketa.com.br>
2022-07-11 15:57:08 -0300

    chore project

--- Exercícios/ex-16/idade2.js
+++
@@ -1,4 +0,0 @@
-const Dias = (anos, meses, dias) =>{
-    let resultado =  (dias+(anos*365)+(meses*30))
-    console.log(`Sua idade em dias é ${resultado}`)
-}
--- Exercícios/ex-9/ex-9.js
+++
@@ -1,9 +0,0 @@
-const verify = (ver) => {
-    if (ver.indexOf('dota') === -1) {
-        return "Não exista o parâmetro 'DOTA'!"
-    }
-    else {
-        return "Existe o parâmetro 'DOTA'"
-    }
-}
-console.log(verify("dota"))
